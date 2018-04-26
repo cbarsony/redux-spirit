1ea462b407d279076b1e3478ea1d8f7f7c1b3ef7
CsabaBarsony <csaba.barsony@gmail.com>
2018-04-26 14:51:43 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,99 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.reduxSpirit = undefined;
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _scionCore = require('scion-core');
+
+exports.reduxSpirit = function reduxSpirit(states, opts) {
+  var sc = void 0;
+  var options = opts || {};
+
+  function isObject(val) {
+    if (val === null) {
+      return false;
+    }
+    return typeof val === 'function' || (typeof val === 'undefined' ? 'undefined' : _typeof(val)) === 'object';
+  }
+
+  function initStatechart(store) {
+    setTimeout(function () {
+      if (Array.isArray(states)) {
+        sc = new _scionCore.Statechart({ states: states });
+      } else if (isObject) {
+        sc = new _scionCore.Statechart(states);
+      }
+
+      var lastEvent = void 0;
+
+      if (options.log && options.verbose) {
+        sc.on('onBigStepBegin', function () {
+          return console.log('big step begin');
+        });
+        sc.on('onBigStepEnd', function () {
+          return console.log('big step end');
+        });
+        sc.on('onSmallStepBegin', function (e) {
+          return console.log('small step begin', e);
+        });
+        sc.on('onSmallStepEnd', function () {
+          return console.log('small step end');
+        });
+      }
+
+      sc.on('onSmallStepBegin', function (e) {
+        return lastEvent = e ? e.data : undefined;
+      });
+
+      sc.on('onEntry', function (state) {
+        var action = _extends({}, lastEvent);
+        action.type = 'a:entry:' + state;
+        options.log && console.log('entry', state, action);
+        store.dispatch(action);
+      });
+
+      sc.on('onExit', function (state) {
+        var action = _extends({}, lastEvent);
+        action.type = 'a:exit:' + state;
+        options.log && console.log('exit', state, action);
+        store.dispatch(action);
+      });
+
+      sc.on('onTransition', function () {
+        if (lastEvent && lastEvent.type) {
+          var action = _extends({}, lastEvent);
+          action.type = 'a:transition:' + lastEvent.type;
+          options.log && console.log('transition', action);
+          store.dispatch(action);
+        }
+      });
+
+      sc.start();
+    }, 0);
+  }
+
+  return function (store) {
+    !sc && initStatechart(store);
+
+    return function (next) {
+      return function (action) {
+        if (!action.type.startsWith('a:')) {
+          if (options.log) console.log('event', action);
+          sc.gen({
+            name: action.type,
+            data: action
+          });
+        } else {
+          if (options.log) console.log('action', action);
+          next(action);
+        }
+      };
+    };
+  };
+};
